ceb71bfab0092d6293d2cbd5d58d1ebb0c97238c
Arsan Gamal <arsan.gamal@hotmail.com>
2025-06-27 16:59:41 +0300

    Update JWT configuration in config and docker-compose files to ensure consistent token expiration settings

--- config.py
+++ config.py
@@ -2,7 +2,14 @@ import os
 
 
 class Config:
+    # SQLAlchemy configuration
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # JWT configuration
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "123456")
+    JWT_ACCESS_TOKEN_EXPIRES = os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 3600)
+    JWT_REFRESH_TOKEN_EXPIRES = os.getenv("JWT_REFRESH_TOKEN_EXPIRES", 86400)
+
+    # Flask configuration
     MAX_PER_PAGE = 10
